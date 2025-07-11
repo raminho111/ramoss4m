2fd0c06613a49d893e589b2d9446a0e43d80af13
raminho111 <krawplay333@gmail.com>
2025-07-11 12:52:14 -0300

    Update keycheck_dylib.m

--- keycheck_dylib.m
+++ keycheck_dylib.m
@@ -1,29 +1,34 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-bool validateKey(NSString *key);
+bool validateKey(NSString *key, NSString *hwid);
 void promptForKey(void);
 
 static bool isPromptShowing = false;
 
 __attribute__((constructor))
 static void initialize() {
+    NSString *hwid = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     NSString *savedKey = [defaults stringForKey:@"user_key"];
-    if (!savedKey || !validateKey(savedKey)) {
+    if (!savedKey || !validateKey(savedKey, hwid)) {
         dispatch_async(dispatch_get_main_queue(), ^{
             promptForKey();
         });
     }
 }
 
-bool validateKey(NSString *key) {
-    // Apenas valida a key, sem HWID
-    NSString *urlString = [NSString stringWithFormat:@"https://keyauth.win/api/1.0/?name=ramoss4m%%20ios&ownerid=wBOrQJSMB8&version=1.0&type=verify&key=%@", key];
+bool validateKey(NSString *key, NSString *hwid) {
+    // Construir URL da API Auth.gg com os parâmetros necessários
+    NSString *urlString = [NSString stringWithFormat:
+        @"https://auth.gg/api/1.0/?type=verify&aid=625185&secret=7cHD2NsVsKQmSNfDd9b9GnvqUYg3olmzUn3&key=%@&hwid=%@",
+        key, hwid];
     NSURL *url = [NSURL URLWithString:urlString];
     NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
     NSData *responseData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
     if (!responseData) return false;
+    
     NSDictionary *json = [NSJSONSerialization JSONObjectWithData:responseData options:0 error:nil];
     return [json[@"success"] boolValue];
 }
@@ -63,7 +68,7 @@ void promptForKey() {
         return;
     }
 
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"FFH4X FFMAX"
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"ramoss4m Auth"
                                                                    message:@"Insira sua key para continuar."
                                                             preferredStyle:UIAlertControllerStyleAlert];
     [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
@@ -72,7 +77,7 @@ void promptForKey() {
 
     UIAlertAction *confirm = [UIAlertAction actionWithTitle:@"Verificar" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
         NSString *inputKey = alert.textFields.firstObject.text;
-        if (validateKey(inputKey)) {
+        if (validateKey(inputKey, [[[UIDevice currentDevice] identifierForVendor] UUIDString])) {
             [[NSUserDefaults standardUserDefaults] setObject:inputKey forKey:@"user_key"];
             [[NSUserDefaults standardUserDefaults] synchronize];
             isPromptShowing = false;
